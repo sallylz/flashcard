629a0938ccc6d9411a092819f81f3d8d28460efa
Sally Zhao <35904566+sallylz@users.noreply.github.com>
2019-10-04 13:27:30 -0700

    Add files via upload

---
+++ public/login.js
@@ -0,0 +1,9 @@
+function myLogin(){
+    url = 'auth/google';
+    xhr = new XMLHttpRequest();
+    xhr.open('GET',url,true);
+    xhr.onload = function () { console.log('logged in!'); };
+    xhr.onerror = function () { console.log('browser sees error');}; 
+    xhr.send();
+}
+
